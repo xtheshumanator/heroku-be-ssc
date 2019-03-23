7a098751934ae4485dc78aaeeddfb3cf2a0cd94b
fatoumeh <fatoumeh@me.com>
2019-03-23 09:15:11 +0000

    fix heroku port issue

--- server.py
+++ server.py
@@ -215,8 +215,8 @@ def handle_update_workspace(workspace_name):
 #
 
 if __name__ == "__main__":
-    # app.run(debug=True)
-    port = int(os.environ.get('PORT', 9090))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
     # app.run(host='0.0.0.0', port=port)
-    app.run(port=port);
+    #app.run(port=port);
 
